de89ce2a8317496ed46960752824ed2b533a7808
alxld <aaronandlouise@yahoo.com>
2022-12-12 01:29:37 +0000

    Changing TV entity name

--- custom_components/living_room_light/light.py
+++ custom_components/living_room_light/light.py
@@ -51,7 +51,7 @@ class LivingRoomLight(NewLight):
         self.entities["light.living_room_group_new"] = None
         self.switch = "Living Room Switch"
         self.motion_sensors.append("Living Room Motion Sensor")
-        self.motion_disable_entities.append("media_player.sony_bravia_tv")
+        self.motion_disable_entities.append("remote.sony_xbr_65a1e")
 
         self.has_brightness_threshold = True
         self.brightness_threshold = 191
